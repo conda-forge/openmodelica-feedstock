6378c2b24c95c7a7c16166f00485a6a21e2f4e84
Julien Schueller <schueller@phimeca.com>
2026-04-24 15:45:53 +0200

    Relative compiler path

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,14 +11,11 @@ sed -i "/omc_add_subdirectory(testsuite)/d" CMakeLists.txt
 # delete 5 last lines to drop tests
 head -n -5 OMCompiler/SimulationRuntime/c/CMakeLists.txt > CMakeLists.txt.new && mv CMakeLists.txt.new OMCompiler/SimulationRuntime/c/CMakeLists.txt
 
-cmake -G "Ninja" -LAH \
-  -DCMAKE_C_COMPILER=`basename ${CC}` \
-  ${CMAKE_ARGS} \
+cmake ${CMAKE_ARGS} -G "Ninja" -LAH \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DOM_ENABLE_GUI_CLIENTS=ON -DOM_QT_MAJOR_VERSION=5 -DOM_OMEDIT_ENABLE_QTWEBENGINE=ON \
   -DOM_OMC_ENABLE_FORTRAN=ON -DOM_OMC_ENABLE_OPTIMIZATION=ON -DOM_OMC_ENABLE_MOO=ON \
   -DOM_USE_CCACHE=OFF \
-  -DBLA_VENDOR=Generic \
   -B build -S .
 cmake --build build --target install --parallel ${CPU_COUNT}
