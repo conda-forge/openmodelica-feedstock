95c20c7edcb92c0aa6015407161b2ad2dee9fec3
Julien Schueller <schueller@phimeca.com>
2026-04-29 19:58:09 +0200

    Update to 1.26.7

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,15 +2,6 @@
 
 set -x
 
-# error: expected '=', ',', ';', 'asm' or '__attribute__' before 'void'
-curl -L https://github.com/OpenModelica/OMCompiler-3rdParty/pull/89.patch | patch -p1 -d OMCompiler/3rdParty
-
-# https://github.com/OpenModelica/OpenModelica/issues/15337
-# add_subdirectory given source "testsuite" which is not an existing
-sed -i "/omc_add_subdirectory(testsuite)/d" CMakeLists.txt
-# delete 5 last lines to drop tests
-head -n -5 OMCompiler/SimulationRuntime/c/CMakeLists.txt > CMakeLists.txt.new && mv CMakeLists.txt.new OMCompiler/SimulationRuntime/c/CMakeLists.txt
-
 cmake ${CMAKE_ARGS} -G "Ninja" -LAH \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
